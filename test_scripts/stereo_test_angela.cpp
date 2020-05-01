34581c43a7455b72d59f9ed8d2dcf58688548aeb
Nithin Subbiah Meganathan <nithinsubbiah@gmail.com>
2020-05-01 13:06:19 -0400

    need to debug test script

--- test_scripts/stereo_test_angela.cpp
+++ test_scripts/stereo_test_angela.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
 #include<string>
@@ -7,15 +8,30 @@
 using namespace cv;
 using namespace std;
 
+void track_features(Mat img0, Mat img1) {
+    vector<KeyPoint> img0_features;
+    int max_corners = 150;
+    double quality = 0.01, min_distance = 30;
+    goodFeaturesToTrack(img0,img0_features,max_corners,quality,min_distance);
+    cout<<img0_features.size();
+}
+
 int main()
 {
-    string img1_file_name = "left.jpg";
-    Mat img1, img2;
+    string img0_file_name = "left.jpg", img1_file_name = "right.jpg";
+    Mat img0, img1;
+    img0 = imread(img0_file_name, CV_LOAD_IMAGE_COLOR);   // Read the file
     img1 = imread(img1_file_name, CV_LOAD_IMAGE_COLOR);   // Read the file
 
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Display window", img1 );                   // Show our image inside it.
+    Mat img0_gray, img1_gray;
+    cvtColor(img0, img0_gray, CV_BGR2GRAY );
+    cvtColor(img1, img1_gray, CV_BGR2GRAY );
 
-    waitKey(0);                                          // Wait for a keystroke in the window
+    track_features(img0_gray,img1_gray);
+
+    // namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    // imshow( "Display window", img1);                   // Show our image inside it.
+
+    // waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
 }
