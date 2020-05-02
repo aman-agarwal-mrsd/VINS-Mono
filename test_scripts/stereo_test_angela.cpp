fdecf622c8b20a955f64664bedfb982c0fe23e47
robohip <akalashi@andrew.cmu.edu>
2020-05-02 15:00:21 -0400

    builds

--- test_scripts/stereo_test_angela.cpp
+++ test_scripts/stereo_test_angela.cpp
@@ -30,7 +30,6 @@ void track_features(Mat img0, Mat img1) {
     //image 1
     vector<Point2f> img1_features;
     vector<KeyPoint> img1_kps;
-
     goodFeaturesToTrack(img1,img1_features,max_corners,quality,min_distance);
     for (int i=0; i<img1_features.size();i++) {
         Point2f pt_to_push1 = img1_features[i];
@@ -44,18 +43,41 @@ void track_features(Mat img0, Mat img1) {
     //run BRIEF descriptor on Image 0
     cv::Mat img0_desc;
     Ptr<cv::xfeatures2d::BriefDescriptorExtractor> brief_img0 =  cv::xfeatures2d::BriefDescriptorExtractor::create(64); //i don't know what 64 means - it was in the example
-    brief_img0->compute(img0.data, img0_kps, img0_desc); //feature_points_depth.points might need to be of type vector<KeyPoint>
+    brief_img0->compute(img0, img0_kps, img0_desc); //feature_points_depth.points might need to be of type vector<KeyPoint>
 
     //run BRIEF descriptor on Image 1
     cv::Mat img1_desc;
     Ptr<cv::xfeatures2d::BriefDescriptorExtractor> brief_img1 =  cv::xfeatures2d::BriefDescriptorExtractor::create(64); //i don't know what 64 means - it was in the example
-    brief_img1->compute(img1.data, img1_kps, img1_desc); //feature_points_depth.points might need to be of type vector<KeyPoint>
+    brief_img1->compute(img1, img1_kps, img1_desc); //feature_points_depth.points might need to be of type vector<KeyPoint>
 
     // match the points
     vector<DMatch> matches;
     Ptr<BFMatcher> desc_matcher = cv::BFMatcher::create(cv::NORM_L2, true); //choose NORM_L1, NORM_L2, NORM_HAMMING, NORM_HAMMING2. 
     desc_matcher->match(img0_desc, img1_desc, matches, Mat());
 
+    Ptr<DescriptorMatcher> flann_matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<DMatch> > flann_matches;
+    flann_matcher->knnMatch(img0_desc, img1_desc, flann_matches, 2 );
+
+
+
+    //-- Filter matches using the Lowe's ratio test
+    const float ratio_thresh = 0.7f;
+    std::vector<DMatch> good_matches;
+    for (size_t i = 0; i < flann_matches.size(); i++)
+    {
+        if (flann_matches[i][0].distance < ratio_thresh * flann_matches[i][1].distance)
+        {
+            good_matches.push_back(flann_matches[i][0]);
+        }
+    }
+    //-- Draw matches
+    Mat img_matches;
+    drawMatches(img0, img0_kps, img1, img1_kps, good_matches, img_matches, Scalar::all(-1),
+                 Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    //-- Show detected matches
+    imshow("Good Matches", img_matches );
+    waitKey();
 }
 
 int main()
